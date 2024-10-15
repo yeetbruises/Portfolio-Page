c4c776df82c4cb6b6f5f1b3024f9f81d4636abf3
vineetsaraf <tcw7711@gmail.com>
2024-10-15 18:33:36 -0400

    Add missing dependecy

--- server/server.js
+++ server/server.js
@@ -14,10 +14,6 @@ app.get("/api", (req, res) => {
     res.json({"users": ["userOne"]})
 });
 
-app.use("/", (req, res) => {
-    res.send("Server is running")
-});
-
 app.use("/images", express.static(path.join(__dirname, '/images')));
 
 app.listen(port, () => console.log("Server started on port " + port));
