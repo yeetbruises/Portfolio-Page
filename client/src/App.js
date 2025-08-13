ea4e8038ea77a258d6b00ccf465fec8828b39857
vineetsaraf <tcw7711@gmail.com>
2025-08-13 12:57:13 -0400

    Alter social media links and main card summary

--- client/src/App.js
+++ client/src/App.js
@@ -254,15 +254,51 @@ function App() {
                                                         This site showcases some of my 
                                                         work. I have a passion for full-stack web development, creative data visuals, and AI. 
                                                         </p>
-                                                        
-                                                        <div className='tools' style={{ marginTop: '10px' }}>
-                                                            <a href='https://www.linkedin.com/in/vineet-saraf'>
-                                                                <img src="/images/badges/linkedin.svg" style={{width: '100px', height: 'auto'}} />
+                                                        <br/>
+                                                        <p>
+                                                        Here's what I'm working on right now:
+                                                        <br/>• Working on fleshing out a CPU simulator, with the goal of running a game on it.
+                                                        <br/>• Starting my Masters Degree at <a href="https://www.gatech.edu/"><b>Georgia Tech</b></a> in Computer Science.
+                                                        <br/>• Studying High Performance Computing Architecture and Databases atm.
+                                                        <br/>• Seeking full-time opportunities for a new graduate.
+                                                        </p>
+                                                        <br />
+                                                        <br />
+                                                        <ul class="Landing_social_ul_hero__bHIN8 social-bar">
+                                                            <li>
+                                                                <a href="https://www.linkedin.com/in/vineet-saraf/"
+                                                                    rel="noopener noreferrer"
+                                                                    title="Open LinkedIn Profile"
+                                                                    class="Landing_here_svg__fGEgQ social-link">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                                                    <path d="M6.94048 4.99993C6.94011 5.81424 6.44608 6.54702 5.69134 6.85273C4.9366 7.15845 4.07187 6.97605 3.5049 6.39155C2.93793 5.80704 2.78195 4.93715 3.1105 4.19207C3.43906 3.44699 4.18654 2.9755 5.00048 2.99993C6.08155 3.03238 6.94097 3.91837 6.94048 4.99993ZM7.00048 8.47993H3.00048V20.9999H7.00048V8.47993ZM13.3205 8.47993H9.34048V20.9999H13.2805V14.4299C13.2805 10.7699 18.0505 10.4299 18.0505 14.4299V20.9999H22.0005V13.0699C22.0005 6.89993 14.9405 7.12993 13.2805 10.1599L13.3205 8.47993Z"/>
+                                                                </svg>
+                                                                </a>
+                                                            </li>
+
+                                                            <li>
+                                                                <a href="https://github.com/yeetbruises"
+                                                                    rel="noopener noreferrer"
+                                                                    title="Open GitHub Profile"
+                                                                    class="Landing_here_svg__fGEgQ social-link">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                                                    <path d="M12.001 2C6.47598 2 2.00098 6.475 2.00098 12C2.00098 16.425 4.86348 20.1625 8.83848 21.4875C9.33848 21.575 9.52598 21.275 9.52598 21.0125C9.52598 20.775 9.51348 19.9875 9.51348 19.15C7.00098 19.6125 6.35098 18.5375 6.15098 17.975C6.03848 17.6875 5.55098 16.8 5.12598 16.5625C4.77598 16.375 4.27598 15.9125 5.11348 15.9C5.90098 15.8875 6.46348 16.625 6.65098 16.925C7.55098 18.4375 8.98848 18.0125 9.56348 17.75C9.65098 17.1 9.91348 16.6625 10.201 16.4125C7.97598 16.1625 5.65098 15.3 5.65098 11.475C5.65098 10.3875 6.03848 9.4875 6.67598 8.7875C6.57598 8.5375 6.22598 7.5125 6.77598 6.1375C6.77598 6.1375 7.61348 5.875 9.52598 7.1625C10.326 6.9375 11.176 6.825 12.026 6.825C12.876 6.825 13.726 6.9375 14.526 7.1625C16.4385 5.8625 17.276 6.1375 17.276 6.1375C17.826 7.5125 17.476 8.5375 17.376 8.7875C18.0135 9.4875 18.401 10.375 18.401 11.475C18.401 15.3125 16.0635 16.1625 13.8385 16.4125C14.201 16.725 14.5135 17.325 14.5135 18.2625C14.5135 19.6 14.501 20.675 14.501 21.0125C14.501 21.275 14.6885 21.5875 15.1885 21.4875C19.259 20.1133 21.9999 16.2963 22.001 12C22.001 6.475 17.526 2 12.001 2Z"/>
+                                                                </svg>
+                                                                </a>
+                                                            </li>
+
+                                                            <li>
+                                                                <a href="#t5"
+                                                                    rel="noopener noreferrer"
+                                                                    title="Send an Email"
+                                                                    class="Landing_here_svg__fGEgQ social-link">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                                                    <path d="M20.7134 8.12811L20.4668 8.69379C20.2864 9.10792 19.7136 9.10792 19.5331 8.69379L19.2866 8.12811C18.8471 7.11947 18.0555 6.31641 17.0677 5.87708L16.308 5.53922C15.8973 5.35653 15.8973 4.75881 16.308 4.57612L17.0252 4.25714C18.0384 3.80651 18.8442 2.97373 19.2761 1.93083L19.5293 1.31953C19.7058 0.893489 20.2942 0.893489 20.4706 1.31953L20.7238 1.93083C21.1558 2.97373 21.9616 3.80651 22.9748 4.25714L23.6919 4.57612C24.1027 4.75881 24.1027 5.35653 23.6919 5.53922L22.9323 5.87708C21.9445 6.31641 21.1529 7.11947 20.7134 8.12811ZM2 4C2 3.44772 2.44772 3 3 3H14V5H4.5052L12 11.662L16.3981 7.75259L17.7269 9.24741L12 14.338L4 7.22684V19H20V11H22V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4Z"/>
+                                                                </svg>
                                                             </a>
-                                                            <a href='https://github.com/yeetbruises'>
-                                                                <img src="/images/badges/github.svg" style={{width: '100px', height: 'auto'}} />
-                                                            </a>
-                                                        </div>
+                                                            </li>
+                                                            </ul>
+
                                                     </div>
                                                 </div>
                                             </>
